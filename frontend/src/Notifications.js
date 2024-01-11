cc4193738a634122ec6b8c0ed2b3ca50dfbe0f8d
Patrick Delaney <patrick.del@gmail.com>
2024-01-11 13:06:05 -0600

    Updating variable for API access.

--- frontend/src/Notifications.js
+++ frontend/src/Notifications.js
@@ -1,3 +1,4 @@
+import { API_URL } from './config';
 import React from 'react';
 import './App.css';
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,2 @@
-const DEV_API_URL = 'http://localhost:8086/';
-const PUBLIC_URL = 'https://brierfoxforecast.ngrok.app';
-const API_URL = 'http://localhost:8089/';
-
-export default { DEV_API_URL, PUBLIC_URL, API_URL };
+export const API_URL = 'https://brierfoxforecast.ngrok.app';
+// export const API_URL = 'http://localhost:8089';
