42a5fa21a9ddc10e314a749721649f5150c3724e
Patrick Delaney <patrick.del@gmail.com>
2024-01-16 12:35:04 -0600

    Setting server back to brierfox.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://brierfoxforecast.ngrok.app';
-export const API_URL = 'http://localhost:8089';
+export const API_URL = 'https://brierfoxforecast.ngrok.app';
+// export const API_URL = 'http://localhost:8089';
