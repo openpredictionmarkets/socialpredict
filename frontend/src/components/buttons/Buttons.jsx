dde13d1d8a0382aff283cf0700db72ae844517c7
Patrick Delaney <patrick.del@gmail.com>
2024-02-25 22:03:50 -0600

    Attempting to centralize colors and refactor to simplify repeatable elements of buttons.

--- frontend/src/components/buttons/Buttons.jsx
+++ frontend/src/components/buttons/Buttons.jsx
@@ -1,51 +1,54 @@
 import React, { useState } from 'react';
+import Colors from '../../config/Colors';
 
 const buttonBaseStyle = `w-full px-4 py-2 text-white border border-transparent rounded focus:outline-none focus:ring-2 focus:ring-offset-2`;
 const yesButtonStyle = `bg-green-500 hover:bg-green-400 focus:ring-green-400`;
 const noButtonStyle = `bg-red-500 hover:bg-red-400 focus:ring-red-400`;
 const yesButtonHoverStyle = `bg-green-600 hover:bg-green-500 focus:ring-green-500`;
 const noButtonHoverStyle = `bg-red-600 hover:bg-red-500 focus:ring-red-500`;
+const boxShadowStyle = '0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08)';
+
 
 const BetYesButton = ({ isSelected }) => (
-  <button
+<button
     className={`${buttonBaseStyle} ${isSelected ? yesButtonStyle : yesButtonHoverStyle}`}
     style={{
-      boxShadow: '0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08)',
-      backgroundColor: isSelected ? '#00bf9a' : '#00cca4',
-      borderColor: isSelected ? '#00bf9a' : '#00cca4'
+        boxShadow: boxShadowStyle,
+        backgroundColor: isSelected ? Colors.green.hover : Colors.green.default,
+        borderColor: isSelected ? Colors.green.default : Colors.green.hover
     }}
     onMouseEnter={(e) => {
-      e.target.style.backgroundColor = isSelected ? '#00cca4' : '#00f2c3';
-      e.target.style.borderColor = isSelected ? '#00cca4' : '#00f2c3';
+        e.target.style.backgroundColor = isSelected ? Colors.green.borderHover : Colors.green.borderDefault;
+        e.target.style.borderColor = isSelected ? Colors.green.borderHover  : Colors.green.borderDefault;
     }}
     onMouseLeave={(e) => {
-      e.target.style.backgroundColor = isSelected ? '#00bf9a' : '#00cca4';
-      e.target.style.borderColor = isSelected ? '#00bf9a' : '#00cca4';
+        e.target.style.backgroundColor = isSelected ? Colors.green.default : Colors.green.hover;
+        e.target.style.borderColor = isSelected ? Colors.green.default : Colors.green.hover;
     }}
-  >
+>
     YES
-  </button>
+</button>
 );
 
 const BetNoButton = ({ isSelected }) => (
-  <button
+<button
     className={`${buttonBaseStyle} ${isSelected ? noButtonStyle : noButtonHoverStyle}`}
     style={{
-      boxShadow: '0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08)',
-      backgroundColor: isSelected ? '#fd5d93' : '#ec250d',
-      borderColor: isSelected ? '#fd5d93' : '#ec250d'
+        boxShadow: boxShadowStyle,
+        backgroundColor: isSelected ? '#fd5d93' : '#ec250d',
+        borderColor: isSelected ? '#fd5d93' : '#ec250d'
     }}
     onMouseEnter={(e) => {
-      e.target.style.backgroundColor = isSelected ? '#ec250d' : '#fd5d93';
-      e.target.style.borderColor = isSelected ? '#ec250d' : '#fd5d93';
+        e.target.style.backgroundColor = isSelected ? '#ec250d' : '#fd5d93';
+        e.target.style.borderColor = isSelected ? '#ec250d' : '#fd5d93';
     }}
     onMouseLeave={(e) => {
-      e.target.style.backgroundColor = isSelected ? '#fd5d93' : '#ec250d';
-      e.target.style.borderColor = isSelected ? '#fd5d93' : '#ec250d';
+        e.target.style.backgroundColor = isSelected ? '#fd5d93' : '#ec250d';
+        e.target.style.borderColor = isSelected ? '#fd5d93' : '#ec250d';
     }}
-  >
+>
     NO
-  </button>
+</button>
 );
 
 export { BetYesButton, BetNoButton };
---
+++ frontend/src/config/Colors.jsx
@@ -0,0 +1,21 @@
+const Colors = {
+    green: {
+        default: "#00bf9a",
+        hover: "#00cca4",
+        borderDefault: "#00f2c3",
+        borderHover: "#00cca4",
+    },
+    red: {
+        default: "#fd5d93",
+        hover: "#ec250d",
+        borderDefault: "#fd5d93",
+        borderHover: "#ec250d",
+    },
+    neutral: {
+        default: "#344675",
+        hover: "#ba54f5",
+        active: "#263148",
+    },
+};
+
+export default Colors;
