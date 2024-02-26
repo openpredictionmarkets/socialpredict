1875cf1d9ba69f5a17fa6935fdf91fcb2395e96f
Patrick Delaney <patrick.del@gmail.com>
2024-02-26 08:28:31 -0600

    Removing Colors.jsx as central styling location, migrating everything to tailwind.config.js

--- frontend/src/components/buttons/Buttons.jsx
+++ frontend/src/components/buttons/Buttons.jsx
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
-import Colors from '../../config/Colors';
+import React from 'react';
 
 const buttonBaseStyle = "w-full px-4 py-2 text-white border border-transparent rounded focus:outline-none focus:ring-2 focus:ring-offset-2";
 // Updated to use Tailwind's custom colors
 const yesButtonStyle = "bg-green-btn hover:bg-green-btn-hover focus:ring-green-btn-border-hover";
 const yesButtonHoverStyle = "bg-green-btn-hover hover:bg-green-btn focus:ring-green-btn-border-default";
-const boxShadowStyle = "shadow-xl"; // Example of using Tailwind's box shadow utility instead of inline style
-
 const noButtonStyle = `bg-red-500 hover:bg-red-400 focus:ring-red-400`;
 const noButtonHoverStyle = `bg-red-600 hover:bg-red-500 focus:ring-red-500`;
-
 
 const BetYesButton = ({ isSelected }) => (
 <button
@@ -22,19 +18,6 @@ const BetYesButton = ({ isSelected }) => (
 const BetNoButton = ({ isSelected }) => (
 <button
     className={`${buttonBaseStyle} ${isSelected ? noButtonStyle : noButtonHoverStyle}`}
-    style={{
-        boxShadow: boxShadowStyle,
-        backgroundColor: isSelected ? '#fd5d93' : '#ec250d',
-        borderColor: isSelected ? '#fd5d93' : '#ec250d'
-    }}
-    onMouseEnter={(e) => {
-        e.target.style.backgroundColor = isSelected ? '#ec250d' : '#fd5d93';
-        e.target.style.borderColor = isSelected ? '#ec250d' : '#fd5d93';
-    }}
-    onMouseLeave={(e) => {
-        e.target.style.backgroundColor = isSelected ? '#fd5d93' : '#ec250d';
-        e.target.style.borderColor = isSelected ? '#fd5d93' : '#ec250d';
-    }}
 >
     NO
 </button>
