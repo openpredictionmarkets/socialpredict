b03824ff95a2b7df8a6e6e4a011b7a11bc53dd42
Patrick Delaney <patrick.del@gmail.com>
2024-02-26 07:50:16 -0600

    Converting colors.jsx to tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-btn': {
+          DEFAULT: '#00bf9a', // Use DEFAULT for the base color
+          hover: '#00cca4',
+          'border-default': '#00f2c3',
+          'border-hover': '#00cca4',
+        },
+        'red-btn': {
+          DEFAULT: '#fd5d93',
+          hover: '#ec250d',
+          'border-default': '#fd5d93',
+          'border-hover': '#ec250d',
+        },
+        'neutral-btn': {
+          DEFAULT: '#344675',
+          hover: '#ba54f5',
+          active: '#263148',
+        },
+      },
+    },
   },
   plugins: [],
 };
