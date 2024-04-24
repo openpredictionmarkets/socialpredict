bdfc76433dcab38f90742554f0c129137e75864c
Patrick Delaney <patrick.del@gmail.com>
2024-04-24 14:34:43 -0500

    Starting to update marketdetails page.

---
+++ frontend/src/pages/marketDetails/MarketDetials.jsx
@@ -0,0 +1,12 @@
+// import { API_URL } from '../../config';
+import React from 'react';
+// import MarketDetailsTable from '../../components/marketDetails/MarketDetailsLayout';
+
+function MarketDetails() {
+    return (
+        <div>hi</div>
+    );
+
+}
+
+export default MarketDetails;
