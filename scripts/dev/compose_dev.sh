e63bad54b18e0be3ede30d1c000b81779f99f84a
Patrick Delaney <patrick.del@gmail.com>
2024-07-15 13:30:58 -0500

    Reverting default password of admin back to password for dev purposes. Adding more logging to comose_dev

--- scripts/dev/compose_dev.sh
+++ scripts/dev/compose_dev.sh
@@ -4,7 +4,8 @@
 [ -z "$CALLED_FROM_SOCIALPREDICT" ] && { echo "Not called from SocialPredict"; exit 42; }
 
 if [ "$1" = "up" ]; then
-	docker compose --env-file $ENV_PATH --file "$SCRIPT_DIR/scripts/docker-compose-dev.yaml" up -d
+	docker compose --env-file $ENV_PATH --file "$SCRIPT_DIR/scripts/docker-compose-dev.yaml" up -d && \
+	echo "SocialPredict may be found at http://localhost . This may take a few seconds to load initially."
 elif [ "$1" = "down" ]; then
 	docker compose --env-file $ENV_PATH --file "$SCRIPT_DIR/scripts/docker-compose-dev.yaml" down
 else
