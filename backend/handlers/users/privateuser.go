592510556b81685e47815c45d6d470291acf48ac
Patrick Delaney <patrick.del@gmail.com>
2024-07-24 23:46:21 -0500

    Updating validate token to use proper httperr returns.

--- backend/handlers/users/privateuser.go
+++ backend/handlers/users/privateuser.go
@@ -45,9 +45,9 @@ func GetPrivateProfileUserResponse(w http.ResponseWriter, r *http.Request) {
 	db := util.GetDB()
 
 	// Validate the token and get the user
-	user, err := middleware.ValidateTokenAndGetUser(r, db)
-	if err != nil {
-		http.Error(w, "Invalid token: "+err.Error(), http.StatusUnauthorized)
+	user, httperr := middleware.ValidateTokenAndGetUser(r, db)
+	if httperr != nil {
+		http.Error(w, "Invalid token: "+httperr.Error(), http.StatusUnauthorized)
 		return
 	}
 
