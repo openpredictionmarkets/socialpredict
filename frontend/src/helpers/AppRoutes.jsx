36663a8e24e1910a3ac89c5b51dbfc5a402e038e
Marko Kovač <124689071+markokovac16@users.noreply.github.com>
2024-08-15 16:02:07 +0200

    Feature/404 page (#235)
    * feat: 404 page setup
    
    404 page now appears when trying to access incorrect links or sections you don't have the permissions for
    
    * style: eliminated vertical scroll due to footer height

--- frontend/src/helpers/AppRoutes.jsx
+++ frontend/src/helpers/AppRoutes.jsx
@@ -1,9 +1,5 @@
 import React, { useEffect } from 'react';
-import {
-    Route,
-    Switch,
-    Redirect,
-} from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { useAuth } from './AuthContent';
 import ChangePassword from '../pages/changepassword/ChangePassword';
 import Profile from '../pages/profile/Profile';
@@ -12,107 +8,116 @@ import Polls from '../pages/polls/Polls';
 import Notifications from '../pages/notifications/Notifications';
 import Create from '../pages/create/Create';
 import About from '../pages/about/About';
-import Home from '../pages/home/Home'
+import Home from '../pages/home/Home';
 import MarketDetails from '../pages/marketDetails/MarketDetails';
 import User from '../pages/user/User';
 import Style from '../pages/style/Style';
 import AdminDashboard from '../pages/admin/AdminDashboard';
+import NotFound from '../pages/notfound/NotFound';
 
 const AppRoutes = () => {
+  const auth = useAuth();
 
-    const auth = useAuth();
+  const isLoggedIn = !!auth.username;
+  const isRegularUser = isLoggedIn && auth.usertype !== 'ADMIN';
+  const mustChangePassword = isLoggedIn && auth.changePasswordNeeded;
 
-    const isLoggedIn = !!auth.username;
-    const isRegularUser = isLoggedIn && auth.usertype !== 'ADMIN';
-    const mustChangePassword = isLoggedIn && auth.changePasswordNeeded;
+  return (
+    <Switch>
+      {/* Stylepage */}
+      <Route exact path='/style' component={Style} />
 
-    return (
-        <Switch>
-            {/* Stylepage */}
-            <Route path='/style' component={Style} />
+      {/* Public Routes */}
+      <Route exact path='/about'>
+        {isLoggedIn && auth.usertype !== 'ADMIN' && mustChangePassword ? (
+          <Redirect to='/changepassword' />
+        ) : (
+          <About />
+        )}
+      </Route>
+      <Route exact path='/markets/:marketId'>
+        {isLoggedIn && auth.usertype !== 'ADMIN' && mustChangePassword ? (
+          <Redirect to='/changepassword' />
+        ) : (
+          <MarketDetails />
+        )}
+      </Route>
+      <Route exact path='/markets'>
+        {isLoggedIn && auth.usertype !== 'ADMIN' && mustChangePassword ? (
+          <Redirect to='/changepassword' />
+        ) : (
+          <Markets />
+        )}
+      </Route>
+      <Route exact path='/polls'>
+        {isLoggedIn && auth.usertype !== 'ADMIN' && mustChangePassword ? (
+          <Redirect to='/changepassword' />
+        ) : (
+          <Polls />
+        )}
+      </Route>
+      <Route exact path='/user/:username'>
+        {isLoggedIn && auth.usertype !== 'ADMIN' && mustChangePassword ? (
+          <Redirect to='/changepassword' />
+        ) : (
+          <User />
+        )}
+      </Route>
 
-            {/* Public Routes */}
-            <Route path='/about'>
-                {isLoggedIn && !auth.usertype === 'ADMIN' && mustChangePassword ? (
-                    <Redirect to='/changepassword' />
-                ) : (
-                    <About />
-                )}
-            </Route>
-            <Route path='/markets/:marketId'>
-            {isLoggedIn && !auth.usertype === 'ADMIN' && mustChangePassword ? (
-                    <Redirect to='/changepassword' />
-                ) : (
-                    <MarketDetails />
-                )}
-            </Route>
-            <Route path='/markets'>
-            {isLoggedIn && !auth.usertype === 'ADMIN' && mustChangePassword ? (
-                    <Redirect to='/changepassword' />
-                ) : (
-                    <Markets />
-                )}
-            </Route>
-            <Route path='/polls'>
-            {isLoggedIn && !auth.usertype === 'ADMIN' && mustChangePassword ? (
-                    <Redirect to='/changepassword' />
-                ) : (
-                    <Polls />
-                )}
-            </Route>
-            <Route path='/user/:username'>
-            {isLoggedIn && !auth.usertype === 'ADMIN' && mustChangePassword ? (
-                    <Redirect to='/changepassword' />
-                ) : (
-                    <User />
-                )}
-            </Route>
+      {/* Private Routes for Regular Users Only */}
+      <Route exact path='/changepassword'>
+        {isRegularUser ? <ChangePassword /> : <Redirect to='/' />}
+      </Route>
+      <Route exact path='/create'>
+        {!isLoggedIn ? (
+          <Redirect to='/' />
+        ) : mustChangePassword ? (
+          <Redirect to='/changepassword' />
+        ) : isRegularUser ? (
+          <Create />
+        ) : (
+          <Redirect to='/' />
+        )}
+      </Route>
+      <Route exact path='/notifications'>
+        {!isLoggedIn ? (
+          <Redirect to='/' />
+        ) : mustChangePassword ? (
+          <Redirect to='/changepassword' />
+        ) : isRegularUser ? (
+          <Notifications />
+        ) : (
+          <Redirect to='/' />
+        )}
+      </Route>
+      <Route exact path='/profile'>
+        {isRegularUser ? <Profile /> : <Redirect to='/' />}
+      </Route>
 
-            {/* Private Routes for Regular Users Only */}
-            <Route path='/changepassword'>
-                {isRegularUser ? <ChangePassword /> : <Home />}
-            </Route>
-            <Route path='/create'>
-                {!isLoggedIn ? (
-                        <Home />
-                    ) : mustChangePassword ? (
-                        <Redirect to='/changepassword' />
-                    ) : isRegularUser ? (
-                        <Create />
-                    ) : (
-                        <Home /> // catch all for all other condtions, not a regular user
-                    )}
-            </Route>
-            <Route path='/notifications'>
-                {!isLoggedIn ? (
-                    <Home />
-                ) : mustChangePassword ? (
-                    <Redirect to='/changepassword' />
-                ) : isRegularUser ? (
-                    <Notifications />
-                ) : (
-                    <Home /> // catch all for all other condtions, not a regular user
-                )}
-            </Route>
-            <Route path='/profile'>
-                {isRegularUser ? <Profile /> : <Home />}
-            </Route>
-            {/* Admin Routes */}
-            <Route path='/admin'>
-                {isLoggedIn && auth.usertype === 'ADMIN' ? <AdminDashboard /> : <Home />}
-            </Route>
+      {/* Admin Routes */}
+      <Route exact path='/admin'>
+        {isLoggedIn && auth.usertype === 'ADMIN' ? (
+          <AdminDashboard />
+        ) : (
+          <Redirect to='/' />
+        )}
+      </Route>
 
-            {/* If no other route matches, redirect to home */}
-            <Route path='/'>
-            {isLoggedIn && !auth.usertype === 'ADMIN' && mustChangePassword ? (
-                    <Redirect to='/changepassword' />
-                ) : (
-                    <Home />
-                )}
-            </Route>
+      {/* Home Route */}
+      <Route exact path='/'>
+        {isLoggedIn && auth.usertype !== 'ADMIN' && mustChangePassword ? (
+          <Redirect to='/changepassword' />
+        ) : (
+          <Home />
+        )}
+      </Route>
 
-        </Switch>
-    );
+      {/* 404 Route - This should be the last route */}
+      <Route path='*'>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
 };
 
 export default AppRoutes;
