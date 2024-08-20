42c722556b3a2828f3582fc29922b87a9a2cf1de
Arthur Lacey <25300182+ajlacey@users.noreply.github.com>
2024-08-20 01:07:35 -0400

    Remove ValidateBuy from betutils/validatebet.go

--- backend/handlers/bets/betutils/validatebet.go
+++ backend/handlers/bets/betutils/validatebet.go
@@ -7,33 +7,6 @@ import (
 	"gorm.io/gorm"
 )
 
-func ValidateBuy(db *gorm.DB, bet *models.Bet) error {
-	var user models.User
-	var market models.Market
-
-	// Check if username exists
-	if err := db.First(&user, "username = ?", bet.Username).Error; err != nil {
-		return errors.New("invalid username")
-	}
-
-	// Check if market exists and is open
-	if err := db.First(&market, "id = ? AND is_resolved = false", bet.MarketID).Error; err != nil {
-		return errors.New("invalid or closed market")
-	}
-
-	// Check for valid amount: it should be greater than or equal to 1
-	if bet.Amount < 1 {
-		return errors.New("Buy amount must be greater than or equal to 1")
-	}
-
-	// Validate bet outcome: it should be either 'YES' or 'NO'
-	if bet.Outcome != "YES" && bet.Outcome != "NO" {
-		return errors.New("bet outcome must be 'YES' or 'NO'")
-	}
-
-	return nil
-}
-
 func ValidateSale(db *gorm.DB, bet *models.Bet) error {
 	var user models.User
 	var market models.Market
