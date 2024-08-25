e5a74d01b01f60ef0471f56441275a459414c0d8
Patrick Delaney <patrick.del@gmail.com>
2024-08-25 07:21:32 -0500

    Creating setuphandler to allow in-game set up to be queried via API (#274)
    * Updating draft handler.
    
    * Update handler with test function.
    
    * Tested working endpoint and golang test function for happy case.
    
    * Updating, removing comment.

---
+++ backend/handlers/setup/setuphandler.go
@@ -0,0 +1,22 @@
+package setup
+
+import (
+	"encoding/json"
+	"net/http"
+	"socialpredict/setup"
+)
+
+func GetSetupHandler(w http.ResponseWriter, r *http.Request) {
+
+	appConfig, err := setup.LoadEconomicsConfig()
+	if err != nil {
+		http.Error(w, "Failed to load economic config", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(appConfig.Economics)
+	if err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
+}
