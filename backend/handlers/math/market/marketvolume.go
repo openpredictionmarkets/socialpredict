166712856f66d954d73f402f28b92d08df58dce3
alacey <25300182+ajlacey@users.noreply.github.com>
2024-09-07 01:21:31 -0400

    Move app config to GetEndMarketVolume

--- backend/handlers/math/market/marketvolume.go
+++ backend/handlers/math/market/marketvolume.go
@@ -5,17 +5,8 @@ import (
 	"socialpredict/setup"
 )
 
-// appConfig holds the loaded application configuration accessible within the package
-var appConfig *setup.EconomicConfig
-
-func init() {
-	// Load configuration
-	appConfig = setup.MustLoadEconomicsConfig()
-}
-
-// getMarketVolume returns the total volume of trades for a given market
+// GetMarketVolume returns the total volume of trades for a given market
 func GetMarketVolume(bets []models.Bet) int64 {
-
 	var totalVolume int64
 	for _, bet := range bets {
 		totalVolume += bet.Amount
@@ -29,7 +20,8 @@ func GetMarketVolume(bets []models.Bet) int64 {
 // returns the market volume + subsidization added into pool,
 // subsidzation in pool could be paid out after resolution but not sold mid-market
 func GetEndMarketVolume(bets []models.Bet) int64 {
-
-	return GetEndMarketVolume(bets) + appConfig.Economics.MarketCreation.InitialMarketSubsidization
+	//GetEndMarketVolume is currently unused
+	appConfig := setup.MustLoadEconomicsConfig()
+	return GetMarketVolume(bets) + appConfig.Economics.MarketCreation.InitialMarketSubsidization
 
 }
