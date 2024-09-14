1bed242188844572cfe915e78c21a7cf5db1fe4f
alacey <25300182+ajlacey@users.noreply.github.com>
2024-09-14 13:07:36 -0400

    Use market creation loader in userpositionmarkethandler

--- backend/handlers/users/userpositiononmarkethandler.go
+++ backend/handlers/users/userpositiononmarkethandler.go
@@ -5,29 +5,32 @@ import (
 	"net/http"
 	"socialpredict/handlers/positions"
 	"socialpredict/middleware"
+	"socialpredict/setup"
 	"socialpredict/util"
 
 	"github.com/gorilla/mux"
 )
 
-func UserMarketPositionHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	marketId := vars["marketId"]
+func UserMarketPositionHandler(mcl setup.MarketCreationLoader) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		marketId := vars["marketId"]
 
-	// Open up database to utilize connection pooling
-	db := util.GetDB()
-	user, httperr := middleware.ValidateTokenAndGetUser(r, db)
-	if httperr != nil {
-		http.Error(w, "Invalid token: "+httperr.Error(), http.StatusUnauthorized)
-		return
+		// Open up database to utilize connection pooling
+		db := util.GetDB()
+		user, httperr := middleware.ValidateTokenAndGetUser(r, db)
+		if httperr != nil {
+			http.Error(w, "Invalid token: "+httperr.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		userPosition, err := positions.CalculateMarketPositionForUser_WPAM_DBPM(mcl, db, marketId, user.Username)
+		if err != nil {
+			http.Error(w, "Error calculating user market position: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(userPosition)
 	}
-
-	userPosition, err := positions.CalculateMarketPositionForUser_WPAM_DBPM(db, marketId, user.Username)
-	if err != nil {
-		http.Error(w, "Error calculating user market position: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(userPosition)
 }
