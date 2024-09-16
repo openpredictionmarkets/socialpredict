8a7c5e7b01c7fd1baf00ec692dc37a26306384bd
Arthur <25300182+ajlacey@users.noreply.github.com>
2024-09-15 23:44:51 -0400

    Update the place bet handler to take an EconConfigLoader (#309)
    Co-authored-by: Patrick Delaney <patrick.del@gmail.com>

--- backend/handlers/bets/placebethandler.go
+++ backend/handlers/bets/placebethandler.go
@@ -2,7 +2,6 @@ package betshandlers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	betutils "socialpredict/handlers/bets/betutils"
 	"socialpredict/logging"
@@ -13,91 +12,82 @@ import (
 	"time"
 )
 
-// appConfig holds the loaded application configuration accessible within the package
-var appConfig *setup.EconomicConfig
+func PlaceBetHandler(loadEconConfig setup.EconConfigLoader) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Only allow POST requests
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
+			return
+		}
 
-func init() {
-	var err error
-	appConfig, err = setup.LoadEconomicsConfig()
-	if err != nil {
-		log.Fatalf("Failed to load configuration: %v", err)
+		// Validate JWT token and extract user information
+		db := util.GetDB() // Get the database connection
+		user, httperr := middleware.ValidateUserAndEnforcePasswordChangeGetUser(r, db)
+		if httperr != nil {
+			http.Error(w, httperr.Error(), httperr.StatusCode)
+			return
+		}
+
+		var betRequest models.Bet
+		// Decode the request body into betRequest
+		err := json.NewDecoder(r.Body).Decode(&betRequest)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Validate the request (check if market exists, if not closed/resolved, etc.)
+		betutils.CheckMarketStatus(db, betRequest.MarketID)
+
+		// sum up fees
+		sumOfBetFees := betutils.GetBetFees(db, user, betRequest)
+		logging.LogAnyType(sumOfBetFees, "sumOfBetFees")
+
+		appConfig := loadEconConfig()
+		// Check if the user has enough balance to place the bet
+		// Use the appConfig for configuration values
+		maximumDebtAllowed := appConfig.Economics.User.MaximumDebtAllowed
+
+		// Check if the user's balance after the bet would be lower than the allowed maximum debt
+		// deduct fees along with calculation to ensure fees can be paid.
+		if user.AccountBalance-betRequest.Amount-sumOfBetFees < -maximumDebtAllowed {
+			http.Error(w, "Insufficient balance", http.StatusBadRequest)
+			return
+		}
+
+		// make entry for user.AccountBalance
+		user.AccountBalance = user.AccountBalance - betRequest.Amount - sumOfBetFees
+
+		// Update the user's balance in the database
+		if err := db.Save(&user).Error; err != nil {
+			http.Error(w, "Error updating user balance: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Create a new Bet object
+		bet := models.Bet{
+			Username: user.Username,
+			MarketID: betRequest.MarketID,
+			Amount:   betRequest.Amount,
+			PlacedAt: time.Now(), // Set the current time as the placement time
+			Outcome:  betRequest.Outcome,
+		}
+
+		// Validate the final bet before putting into database
+		if err := betutils.ValidateBuy(db, &bet); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Save the Bet to the database, if transaction was greater than 0.
+		result := db.Create(&bet)
+		if result.Error != nil {
+			http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Return a success response
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(bet)
 	}
-}
-
-func PlaceBetHandler(w http.ResponseWriter, r *http.Request) {
-	// Only allow POST requests
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
-		return
-	}
-
-	// Validate JWT token and extract user information
-	db := util.GetDB() // Get the database connection
-	user, httperr := middleware.ValidateUserAndEnforcePasswordChangeGetUser(r, db)
-	if httperr != nil {
-		http.Error(w, httperr.Error(), httperr.StatusCode)
-		return
-	}
-
-	var betRequest models.Bet
-	// Decode the request body into betRequest
-	err := json.NewDecoder(r.Body).Decode(&betRequest)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Validate the request (check if market exists, if not closed/resolved, etc.)
-	betutils.CheckMarketStatus(db, betRequest.MarketID)
-
-	// sum up fees
-	sumOfBetFees := betutils.GetBetFees(db, user, betRequest)
-
-	logging.LogAnyType(sumOfBetFees, "sumOfBetFees")
-
-	// Check if the user has enough balance to place the bet
-	// Use the appConfig for configuration values
-	maximumDebtAllowed := appConfig.Economics.User.MaximumDebtAllowed
-
-	// Check if the user's balance after the bet would be lower than the allowed maximum debt
-	// deduct fees along with calculation to ensure fees can be paid.
-	if user.AccountBalance-betRequest.Amount-sumOfBetFees < -maximumDebtAllowed {
-		http.Error(w, "Insufficient balance", http.StatusBadRequest)
-		return
-	}
-
-	// make entry for user.AccountBalance
-	user.AccountBalance = user.AccountBalance - betRequest.Amount - sumOfBetFees
-
-	// Update the user's balance in the database
-	if err := db.Save(&user).Error; err != nil {
-		http.Error(w, "Error updating user balance: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Create a new Bet object
-	bet := models.Bet{
-		Username: user.Username,
-		MarketID: betRequest.MarketID,
-		Amount:   betRequest.Amount,
-		PlacedAt: time.Now(), // Set the current time as the placement time
-		Outcome:  betRequest.Outcome,
-	}
-
-	// Validate the final bet before putting into database
-	if err := betutils.ValidateBuy(db, &bet); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Save the Bet to the database, if transaction was greater than 0.
-	result := db.Create(&bet)
-	if result.Error != nil {
-		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Return a success response
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(bet)
 }
