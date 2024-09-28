d310deb4306fc89b9509b51ae5cc0d750a08672d
AnhQuan Nguyen <j4qfrost@gmail.com>
2024-09-28 10:20:19 -0700

    tagging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,6 @@ function ErrorFallback({ error, resetErrorBoundary }) {
         onClick={resetErrorBoundary}
         className='px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors'
       >
-        Try again
       </button>
     </div>
   );
