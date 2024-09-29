09fd7f83d5e2ec206e78a5dbdc132d3f2fa15422
alacey <25300182+ajlacey@users.noreply.github.com>
2024-09-29 00:26:50 -0400

    Remove PrivateUserResponse type for redundancy
    replace it with models.PrivateUser call.
    remove helper for getting private info out of the user type.
    directly embed the private fields into the response.

--- backend/handlers/users/privateuser.go
+++ backend/handlers/users/privateuser.go
@@ -6,20 +6,11 @@ import (
 	"socialpredict/middleware"
 	"socialpredict/models"
 	"socialpredict/util"
-
-	"gorm.io/gorm"
 )
-
-// PrivateUserResponse is a struct for user data that is safe to send to the client for login
-type PrivateUserResponse struct {
-	Email  string `json:"email"`
-	ApiKey string `json:"apiKey,omitempty"`
-}
 
 type CombinedUserResponse struct {
 	// Private fields
-	Email  string `json:"email"`
-	ApiKey string `json:"apiKey,omitempty"`
+	models.PrivateUser
 	// Public fields
 	Username              string `json:"username"`
 	DisplayName           string `json:"displayname"`
@@ -35,12 +26,6 @@ type CombinedUserResponse struct {
 }
 
 func GetPrivateProfileUserResponse(w http.ResponseWriter, r *http.Request) {
-	// accept get requests
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method is not supported.", http.StatusMethodNotAllowed)
-		return
-	}
-
 	// Use database connection
 	db := util.GetDB()
 
@@ -55,12 +40,10 @@ func GetPrivateProfileUserResponse(w http.ResponseWriter, r *http.Request) {
 	username := user.Username
 
 	publicInfo := GetPublicUserInfo(db, username)
-	privateInfo := GetPrivateUserInfo(db, username)
 
 	response := CombinedUserResponse{
 		// Private fields
-		Email:  privateInfo.Email,
-		ApiKey: privateInfo.ApiKey,
+		PrivateUser: user.PrivateUser,
 		// Public fields
 		Username:              publicInfo.Username,
 		DisplayName:           publicInfo.DisplayName,
@@ -77,15 +60,4 @@ func GetPrivateProfileUserResponse(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
-}
-
-// Function to get the Info From the Database
-func GetPrivateUserInfo(db *gorm.DB, username string) PrivateUserResponse {
-	var user models.User
-	db.Where("username = ?", username).First(&user)
-
-	return PrivateUserResponse{
-		Email:  user.Email,
-		ApiKey: user.ApiKey,
-	}
 }
