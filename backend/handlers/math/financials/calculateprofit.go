03efdb660ed144a19db6dbbe6b0255abcf0a5468
Patrick Delaney <patrick.del@gmail.com>
2024-10-09 16:47:20 -0500

    Update with psuedocode on how we're going to achieve this.

--- backend/handlers/math/financials/calculateprofit.go
+++ backend/handlers/math/financials/calculateprofit.go
@@ -1,1 +1,17 @@
 package financials
+
+// profit = selling price - cost price
+// cost price = amount bet on particular market
+// selling price = current position calculated
+
+// profit(user(market)) -> individual user and market
+// profit(market(users)) -> function that shows all user profits on a market
+// profit(user(markets)) -> function that sums up for a user, all profits on markets they are in
+
+// If we want to build a market leaderboard we need profit(user(market))
+// For all users on a market, calculate profit(user(market))
+// When done, rank and display them in order, greatest (+) to least (-)
+
+func userMarketProfit(username string, marketId string) int64 {
+
+}
