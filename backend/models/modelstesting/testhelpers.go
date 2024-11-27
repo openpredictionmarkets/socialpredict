fa52b6729af97b70f00e43a2ec2fa651f2161ce4
Patrick Delaney <patrick.del@gmail.com>
2024-11-27 07:50:15 -0600

    Adding Test Coverage to Positions Math (#383)
    * Adding a noop change to be able to create pull request.
    
    * Refactoring and expanding test coverage to include test on single share allocator.
    
    * Add TestCalculateMarketPositions_WPAM_DBPM
    
    * Adding TestCalculateMarketPositionForUser_WPAM_DBPM
    
    * Adding TestCheckOppositeSharesOwned
    
    * Removing NOOP change.
    
    * Adding additional test coverage.
    
    * Taking initial subsidy out of original calculated volume.
    
    * Expanding function into smaller functions.
    
    * Adding passing tests.
    
    * Adding large marketshares test scenario for divide up market shares.
    
    * Updating positions test coverage.
    
    * Adding a large test case so that we can combine specific test cases with an overall global test between the functions.
    
    * Updating test cases using advice from golang test conventions.
    
    * Adding sequential bet tests to improve logic.
    
    * Adding YES and NO resolution cases.
    
    * Adding correct expected amounts for test case.
    
    * Calculating scaled payouts working now.
    
    * Fixing value to 4.5673076923076925
    
    * excess calculation tests working.
    
    * Testing positive excess.
    
    * Successful negative excess test.
    
    * Adjusted payouts working.
    
    * Passing test for positions calculation.
    
    * Removing hold.txt, simplifying test case and moving function to private.
    
    * Further refactoring, eliminating un-needed test and function: checking opposite share owned.
    
    * Separating variables in type definition by line.
    
    * singleshareallocator should not be able to return no outcome result.
    
    * Fix typo in comments.
    
    * Moving package from utils to modelstesting.
    
    * Updating per appropriate level of verbosity.
    
    * Moving function to modelstesting.
    
    * Use log rather than print to reduce excess logging.
    
    * Eliminat printing of errors in favor of logging.
    
    * Test name removed from test output in case of error.
    
    * Fix mispelling in comments.
    
    * Pulling R out of equaton.
    
    * Getting rid of a few _ notation as well as pulling R out of loop
    
    * Updating comment
    
    * Updating with fatal condition for single share allocator.
    
    * Updating with better error handling for testing singleShareAllocator, improving comment.
    
    * Updating notation.
    
    * Adjusting logic for negative excess to be more efficient.
    
    * Adjusting positive adjustment logic.

---
+++ backend/models/modelstesting/testhelpers.go
@@ -0,0 +1,28 @@
+package modelstesting
+
+import (
+	"socialpredict/handlers/math/probabilities/wpam"
+	"socialpredict/models"
+	"time"
+)
+
+// GenerateBet is used for Generating fake bets for testing purposes
+// helper function to generate bets succinctly for testing
+func GenerateBet(amount int64, outcome, username string, marketID uint, offset time.Duration) models.Bet {
+	return models.Bet{
+		Amount:   amount,
+		Outcome:  outcome,
+		Username: username,
+		PlacedAt: time.Now().Add(offset),
+		MarketID: marketID,
+	}
+}
+
+// helper function to create wpam.ProbabilityChange points succinctly
+func GenerateProbability(probabilities ...float64) []wpam.ProbabilityChange {
+	var changes []wpam.ProbabilityChange
+	for _, p := range probabilities {
+		changes = append(changes, wpam.ProbabilityChange{Probability: p})
+	}
+	return changes
+}
