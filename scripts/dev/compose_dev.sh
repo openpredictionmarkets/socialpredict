74faa8c5b29184e3848cc42ba5c0b805d8b77c05
Jordan George <jordanngeorge@gmail.com>
2025-09-18 22:17:21 -0500

    Add project name to docker compose up command

--- scripts/dev/compose_dev.sh
+++ scripts/dev/compose_dev.sh
@@ -4,7 +4,9 @@
 [ -z "$CALLED_FROM_SOCIALPREDICT" ] && { echo "Not called from SocialPredict"; exit 42; }
 
 if [ "$1" = "up" ]; then
-	docker compose --env-file $ENV_PATH --file "$SCRIPT_DIR/scripts/docker-compose-dev.yaml" up -d && \
+	docker compose \
+		-p socialpredict_dev \
+		--env-file $ENV_PATH --file "$SCRIPT_DIR/scripts/docker-compose-dev.yaml" up -d && \
 	echo "SocialPredict may be found at http://localhost . This may take a few seconds to load initially."
   	echo "Here are the initial settings. These can be changed in setup.yaml"
 	cat $SCRIPT_DIR/backend/setup/setup.yaml
