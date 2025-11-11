c32553cbef5329975a53dc489c08d14097a7cd23
Osnat Katz Moon <osnatk96@gmail.com>
2025-11-11 16:09:20 +0700

    Changes to be committed: 	modified:   frontend/src/utils/chartFormatter.js
    testing to follow

--- frontend/src/utils/chartFormatter.js
+++ frontend/src/utils/chartFormatter.js
@@ -31,6 +31,14 @@ export const createFormatter = (sigFigs) => (rawValue) => {
   return numericValue.toPrecision(sigFigs);
 };
 
+const buildFormatterState = (sigFigs) => ({
+  sigFigs,
+  format: createFormatter(sigFigs),
+});
+
+let cachedFormatter = buildFormatterState(DEFAULT_SIG_FIGS);
+let formatterPromise = null;
+
 export const loadChartFormatter = async () => {
   let requestedSigFigs = DEFAULT_SIG_FIGS;
 
@@ -50,14 +58,35 @@ export const loadChartFormatter = async () => {
   }
 
   const sigFigs = clampSigFigs(requestedSigFigs);
-  return {
-    sigFigs,
-    format: createFormatter(sigFigs),
-  };
+  return buildFormatterState(sigFigs);
+};
+
+export const ensureChartFormatter = () => {
+  if (!formatterPromise) {
+    formatterPromise = loadChartFormatter().then((formatter) => {
+      cachedFormatter = formatter;
+      return formatter;
+    });
+  }
+  return formatterPromise;
+};
+
+export const getChartFormatter = () => cachedFormatter;
+
+export const formatChartValue = (value) => cachedFormatter.format(value);
+
+export const SocialPredictChartFormatter = {
+  ensure: ensureChartFormatter,
+  format: formatChartValue,
+  getFormatter: getChartFormatter,
+  getSigFigs: () => cachedFormatter.sigFigs,
 };
 
 export const SocialPredictChartTools = {
   clampSigFigs,
   createFormatter,
   loadChartFormatter,
+  ensureChartFormatter,
+  formatChartValue,
+  getChartFormatter,
 };
