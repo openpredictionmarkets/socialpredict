713c9dc3b101ead5d5d536aa56f27c661809a989
Patrick W. Delaney <patrick@Mac.hsd1.tx.comcast.net>
2026-01-05 09:06:26 -0600

    Simplifying positionsmath, valuation, wpam

--- backend/internal/domain/math/probabilities/wpam/wpam_current.go
+++ backend/internal/domain/math/probabilities/wpam/wpam_current.go
@@ -1,6 +1,8 @@
 package wpam
 
 func GetCurrentProbability(probChanges []ProbabilityChange) float64 {
-
+	if len(probChanges) == 0 {
+		return 0
+	}
 	return probChanges[len(probChanges)-1].Probability
 }
